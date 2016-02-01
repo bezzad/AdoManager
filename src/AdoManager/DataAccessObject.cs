3e86998c762affd5ae4faaa75d3c2cb6c10456b8
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2016-02-01 19:46:05 +0330

    add dapper to dynamic query

--- src/AdoManager/DataAccessObject.cs
+++ src/AdoManager/DataAccessObject.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace AdoManager
 {
@@ -11,12 +12,12 @@ namespace AdoManager
     {
         public static ConnectionManager Conn = ConnectionManager.GetDefaultConnection();
 
-        public static DataTable GetFrom(string table, params Condition[] conditions)
+        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, params Condition[] conditions)
         {
-            return GetFrom(table, new string[] { }, conditions);
+            return await GetFromAsync(table, new string[] { }, conditions);
         }
 
-        public static DataTable GetFrom(string table, string[] columns, params Condition[] conditions)
+        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, string[] columns, params Condition[] conditions)
         {
             var cols = columns.Any() ? string.Join(", ", columns) : "*";
 
@@ -27,7 +28,14 @@ namespace AdoManager
                 query += $@"WHERE {conditions.ConvertToQuery()}";
             }
 
-            var result = Conn.ExecuteDataSet(query);
+            var result = await Conn.SqlConn.QueryAsync<dynamic>(query);
+
+            return result;
+        }
+
+        public static async Task<IEnumerable<dynamic>> GetFromQueryAsync(string query)
+        {
+            var result = await Conn.SqlConn.QueryAsync<dynamic>(query);
 
             return result;
         }
