624592c7538fd0d2cbc717babad7b7667fc646e4
BehzadKhosravifar <behzadkh@hotmail.com>
2016-05-12 15:34:45 +0430

    Nuget.nuspec added to project

--- src/AdoManager/DataAccessObject.cs
+++ src/AdoManager/DataAccessObject.cs
@@ -10,29 +10,20 @@ namespace AdoManager
     public static class DataAccessObject
     {
         public static ConnectionManager Conn = ConnectionManager.GetDefaultConnection();
-        
+        private static Dictionary<string, object> _cachingQueryResults = new Dictionary<string, object>();
+
         #region Sync Methods
 
-
-        public static IEnumerable<dynamic> GetFrom(string table, params Condition[] conditions)
+        public static IEnumerable<dynamic> GetFrom(string table, bool caching = false, params Condition[] conditions)
         {
-            return GetFrom(table, new string[] { }, conditions);
+            return GetFrom(table, new string[] { }, caching, conditions);
         }
-        
-        public static IEnumerable<dynamic> GetFrom(string table, string[] columns, params Condition[] conditions)
+
+        public static IEnumerable<dynamic> GetFrom(string table, string[] columns, bool caching = false, params Condition[] conditions)
         {
-            var cols = columns.Any() ? string.Join(", ", columns) : "*";
+            string query = GetFormatedQuery(table, columns, conditions);
 
-            string query = $"SELECT {cols} FROM Xomorod.dbo.{table} ";
-
-            if (conditions.Any())
-            {
-                query += $@"WHERE {conditions.ConvertToQuery()}";
-            }
-
-            var result = Conn.SqlConn.Query<dynamic>(query);
-
-            return result;
+            return GetFromQuery(query, caching);
         }
 
         public static IEnumerable<dynamic> GetFromQuery(string query, ExpandoObject param)
@@ -42,37 +33,55 @@ namespace AdoManager
             return result;
         }
 
-
-        #endregion
-        
-        #region Async Methods
-
-        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, params Condition[] conditions)
+        public static IEnumerable<dynamic> GetFromQuery(string query, bool caching = false)
         {
-            return await GetFromAsync(table, new string[] { }, conditions);
-        }
-
-        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, string[] columns, params Condition[] conditions)
-        {
-            var cols = columns.Any() ? string.Join(", ", columns) : "*";
-
-            string query = $"SELECT {cols} FROM Xomorod.dbo.{table} ";
-
-            if (conditions.Any())
+            if (caching)
             {
-                query += $@"WHERE {conditions.ConvertToQuery()}";
+                if (!_cachingQueryResults.ContainsKey(query))
+                {
+                    _cachingQueryResults[query] = Conn.SqlConn.Query<dynamic>(query);
+                }
+                return (IEnumerable<dynamic>)_cachingQueryResults[query];
             }
 
-            var result = await Conn.SqlConn.QueryAsync<dynamic>(query);
+            return Conn.SqlConn.Query<dynamic>(query);
+        }
+
+        public static T ExecuteScalar<T>(string query, ExpandoObject param)
+        {
+            var result = Conn.SqlConn.ExecuteScalar<T>(query, param);
 
             return result;
         }
 
-        public static async Task<IEnumerable<dynamic>> GetFromQueryAsync(string query)
+        public static T ExecuteScalar<T>(string query, bool caching = false)
         {
-            var result = await Conn.SqlConn.QueryAsync<dynamic>(query);
+            if (caching)
+            {
+                if (!_cachingQueryResults.ContainsKey(query))
+                {
+                    _cachingQueryResults[query] = Conn.SqlConn.ExecuteScalar<T>(query);
+                }
+                return (T)_cachingQueryResults[query];
+            }
 
-            return result;
+            return Conn.SqlConn.ExecuteScalar<T>(query);
+        }
+
+        #endregion
+
+        #region Async Methods
+
+        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, bool caching = false, params Condition[] conditions)
+        {
+            return await GetFromAsync(table, new string[] { }, caching, conditions);
+        }
+
+        public static async Task<IEnumerable<dynamic>> GetFromAsync(string table, string[] columns, bool caching = false, params Condition[] conditions)
+        {
+            string query = GetFormatedQuery(table, columns, conditions);
+
+            return await GetFromQueryAsync(query, caching);
         }
 
         public static async Task<IEnumerable<dynamic>> GetFromQueryAsync(string query, ExpandoObject param)
@@ -82,6 +91,60 @@ namespace AdoManager
             return result;
         }
 
+        public static async Task<IEnumerable<dynamic>> GetFromQueryAsync(string query, bool caching = false)
+        {
+            if (caching)
+            {
+                if (!_cachingQueryResults.ContainsKey(query))
+                {
+                    _cachingQueryResults[query] = await Conn.SqlConn.QueryAsync<dynamic>(query);
+                }
+                return (IEnumerable<dynamic>)_cachingQueryResults[query];
+            }
+
+            return await Conn.SqlConn.QueryAsync<dynamic>(query);
+        }
+
+        public static async Task<T> ExecuteScalarAsync<T>(string query, ExpandoObject param)
+        {
+            var result = await Conn.SqlConn.ExecuteScalarAsync<T>(query, param);
+
+            return result;
+        }
+
+        public static async Task<T> ExecuteScalarAsync<T>(string query, bool caching = false)
+        {
+            if (caching)
+            {
+                if (!_cachingQueryResults.ContainsKey(query))
+                {
+                    _cachingQueryResults[query] = await Conn.SqlConn.ExecuteScalarAsync<T>(query);
+                }
+                return (T)_cachingQueryResults[query];
+            }
+
+            return await Conn.SqlConn.ExecuteScalarAsync<T>(query);
+        }
+
         #endregion
+
+        private static string GetFormatedQuery(string table, string[] columns, Condition[] conditions)
+        {
+            var cols = columns.Any() ? string.Join(", ", columns) : "*";
+
+            string query = $"SELECT {cols} FROM dbo.{table} ";
+
+            if (conditions.Any())
+            {
+                query += $@"WHERE {conditions.ConvertToQuery()}";
+            }
+
+            return query;
+        }
+
+        public static void ClearCache()
+        {
+            _cachingQueryResults.Clear();
+        }
     }
 }
